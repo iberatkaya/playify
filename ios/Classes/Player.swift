e056c915449d9cafa0c3fe4abf2466b057568c3a
iberatkaya <beratkaya1998@gmail.com>
2020-12-19 03:02:35 +0300

    Added 3 new functions and better null checking Added custom errors, better documentation and comments

--- ios/Classes/Player.swift
+++ ios/Classes/Player.swift
@@ -1,57 +1,105 @@
 import Foundation
 import MediaPlayer
 
-@available(iOS 10.1, *)
+@available(iOS 10.3, *)
 public class Player {
+    ///The music player controller instance.
     var player: MPMusicPlayerController = MPMusicPlayerController.systemMusicPlayer
-    //Set the queue with unique store song ids
-    func setQueue(songIDs: [String], startPlaying: Bool) {
+    
+    ///Set the queue with unique song ids.
+    func setQueue(songIDs: [String], startPlaying: Bool?, startID: String?) throws {
+        if let startID = startID {
+            if !songIDs.contains(startID) {
+                throw PlayifyError.runtimeError("songIDs does not contain startID!")
+            }
+        }
+        let songs = getMediaItemsWithIDs(songIDs: songIDs)
+        
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: songs))
+        
+        //If a startID is given, find and set the song as the start item.
+        if let startID = startID {
+            if let startItem = getMediaItemsWithIDs(songIDs: [startID]).first {
+                descriptor.startItem = startItem
+            }
+        }
+        
+        player.setQueue(with: descriptor)
+        
+        if let startPlaying = startPlaying {
+            if(startPlaying){
+                player.prepareToPlay(completionHandler: {error in
+                    if error == nil {
+                        self.play()
+                    }
+                })
+            }
+            else {
+                player.prepareToPlay()
+            }
+            
+        }
+    }
+    
+    ///Get MediaItems via a PersistentID using predicates and queries.
+    private func getMediaItemsWithIDs(songIDs: [String]) -> [MPMediaItem] {
         var songs: [MPMediaItem] = []
         for songID in songIDs {
             let songFilter = MPMediaPropertyPredicate(value: songID, forProperty: MPMediaItemPropertyPersistentID, comparisonType: .equalTo)
             let query = MPMediaQuery(filterPredicates: Set([songFilter]))
-            if(query.items != nil && query.items!.capacity > 0){
-                songs.append(query.items![0])
+            if let items = query.items, let first = items.first {
+                songs.append(first)
             }
         }
-        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: songs))
-        
-        
-        player.setQueue(with: descriptor)
-        
-        if(startPlaying){
-            player.prepareToPlay(completionHandler: {error in
-                if error == nil {
-                    self.play()
-                }
-            })
-        }
-        else {
-            player.prepareToPlay()
-        }
-            
+        return songs
     }
-        
+    
+    ///Skip to the beginning of the queue.
+    func skipToBeginning(){
+        player.skipToBeginning()
+    }
+    
+    ///Prepend songs to the current queue.
+    func prepend(songIDs: [String]){
+        let songs = getMediaItemsWithIDs(songIDs: songIDs)
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: songs))
+        player.prepend(descriptor)
+    }
+    
+    ///Append songs to the current queue.
+    func append(songIDs: [String]){
+        let songs = getMediaItemsWithIDs(songIDs: songIDs)
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: songs))
+        player.append(descriptor)
+    }
+    
+    
+    ///Start seeking forward.
     func seekForward(){
         player.beginSeekingForward()
     }
     
+    ///Start seeking backwards.
     func seekBackward(){
         player.beginSeekingBackward()
     }
     
+    ///Stop seeking.
     func endSeeking(){
         player.endSeeking()
     }
     
+    ///Get the current time of the song.
     func getPlaybackTime() -> Float{
         return Float(player.currentPlaybackTime)
     }
     
+    ///Set the current time of the song.
     func setPlaybackTime(time: Float){
         player.currentPlaybackTime = TimeInterval(time)
     }
     
+    ///Set a shuffle mode.
     func setShuffleMode(mode: String){
         if(mode == "off"){
             player.shuffleMode =  MPMusicShuffleMode.off
@@ -64,6 +112,7 @@ public class Player {
         }
     }
     
+    ///Set a repeat mode.
     func setRepeatMode(mode: String){
         if(mode == "none"){
             player.repeatMode =  MPMusicRepeatMode.none
@@ -76,15 +125,10 @@ public class Player {
         }
     }
     
+    ///Play a song with an ID.
     func playItem(songID: String){
-        var songs: [MPMediaItem] = []
-        let songFilter = MPMediaPropertyPredicate(value: songID, forProperty: MPMediaItemPropertyPersistentID, comparisonType: .equalTo)
-        var query = MPMediaQuery(filterPredicates: Set([songFilter]))
-        if(query.items != nil && query.items!.capacity > 0){
-            songs.append(query.items![0])
-        }
-        
-        var descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: songs))
+        let song = getMediaItemsWithIDs(songIDs: [songID])
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: song))
         
         player.setQueue(with: descriptor)
         player.prepareToPlay(completionHandler: {error in
@@ -94,38 +138,39 @@ public class Player {
         })
     }
     
-    //Play the current queue
+    ///Play the current queue.
     func play(){
         player.play()
     }
     
-    //Pause the current playing song
+    ///Pause the current playing song.
     func pause(){
         player.pause()
     }
     
-    //Skip to the next song in the queue
+    ///Skip to the next song in the queue.
     func next(){
         player.skipToNextItem()
     }
     
-    //Skip to the previous song in the queue
+    ///Skip to the previous song in the queue.
     func previous(){
         player.skipToPreviousItem()
     }
     
-    //Get info about the current playing song
+    ///Get info about the current playing song.
     func nowPlaying() -> MPMediaItem? {
         return player.nowPlayingItem
     }
     
-    //Retrieve all songs in the library
+    ///Retrieve all songs in the library.
     func getAllSongs() -> [MPMediaItem] {
         let songsQuery = MPMediaQuery.songs()
         let songs = songsQuery.items ?? []
         return songs
     }
     
+    ///Check if the player is in the playing state.
     func isPlaying() -> Bool {
         return player.playbackState == MPMusicPlaybackState.playing
     }
