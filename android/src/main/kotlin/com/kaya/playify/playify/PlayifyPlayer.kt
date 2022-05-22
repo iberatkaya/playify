ea52518ad40b0428d31a439130d560b5bfaebbdc
iberatkaya <beratkaya1998@gmail.com>
2022-05-22 13:21:16 -0500

    Added many functions for PlayifyPlayer

--- android/src/main/kotlin/com/kaya/playify/playify/PlayifyPlayer.kt
+++ android/src/main/kotlin/com/kaya/playify/playify/PlayifyPlayer.kt
@@ -1,20 +1,159 @@
 package com.kaya.playify.playify
 
+import android.app.*
 import android.content.Context
+import android.content.Intent
 import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
+import android.os.IBinder
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.kaya.playify.playify.Classes.Song
 import java.nio.ByteBuffer
 import java.util.*
+import kotlin.Exception
 import kotlin.collections.ArrayList
 
 
-class PlayifyPlayer {
-    val player = MediaPlayer()
+abstract class SongListener {
+    abstract fun onSongChange(song: String);
+}
 
+class PlayifyPlayer: Service() {
+    val player = MediaPlayer().apply {
+//        setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
+    }
+
+    private lateinit var callback: SongListener
+
+    fun setListener(callback: SongListener) {
+        this.callback = callback;
+    }
+
+    private var songQueue: ArrayList<Song> = ArrayList<Song>()
+    private var queueIndex: Int? = null
+
+    private fun getSongFromMediaStore(context: Context, id: String): Song? {
+        val selection = MediaStore.Audio.Media._ID + " == " + id
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.YEAR,
+            MediaStore.Audio.Media.TRACK,
+        )
+
+        val genresProjection = arrayOf(
+            MediaStore.Audio.Genres.NAME
+        )
+
+        val albumArtProjection = arrayOf(
+            MediaStore.Audio.Albums.ALBUM_ART
+        )
+
+        val cursor: Cursor? = context?.getContentResolver()?.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            null
+        )
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val id = cursor.getString(0)
+                val artist = cursor.getString(1)
+                val albumTitle = cursor.getString(2)
+                val songTitle = cursor.getString(3)
+                val path = cursor.getString(4)
+                val playbackDuration = cursor.getString(5)
+                val releaseYearStr = cursor.getString(6)
+                val trackStr = cursor.getString(7)
+                val track = Integer.parseInt(trackStr)
+                val calendar = Calendar.getInstance()
+
+                var releaseYear: Long? = null;
+                if (releaseYearStr != null) {
+                    calendar.set(Integer.parseInt(releaseYearStr), 0, 1)
+                    releaseYear = calendar.timeInMillis
+                }
+
+                val song = Song(
+                    albumTitle = albumTitle,
+                    artist = artist,
+                    songTitle = songTitle,
+                    path = path,
+                    playbackDuration = playbackDuration.toDoubleOrNull(),
+                    releaseDate = releaseYear,
+                    trackNumber = track,
+                    songID = id,
+                )
+
+                val albumArtCursor: Cursor? = context?.getContentResolver()?.query(
+                    MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+                    albumArtProjection,
+                    null,
+                    null,
+                    null
+                )
+
+                albumArtCursor?.moveToNext()
+
+                val albumArt = albumArtCursor?.getString(0)
+
+                var typedIntArray: Array<Int>? = null
+
+                if (albumArt != null) {
+                    val bitmap = BitmapFactory.decodeFile(albumArt)
+
+                    val size: Int = bitmap.getRowBytes() * bitmap.getHeight()
+                    val byteBuffer: ByteBuffer = ByteBuffer.allocate(size)
+                    bitmap.copyPixelsToBuffer(byteBuffer)
+                    val intBuffer = byteBuffer.asIntBuffer()
+                    val intArray = IntArray(intBuffer.limit())
+
+                    intBuffer.get(intArray)
+                    typedIntArray = intArray.toTypedArray()
+                }
+
+                song.image = typedIntArray
+
+                //Get the genre
+                val genreUri = MediaStore.Audio.Genres.getContentUriForAudioId(
+                    "external",
+                    Integer.parseInt(id)
+                )
+
+                val genreCursor: Cursor? = context?.getContentResolver()?.query(
+                    genreUri,
+                    genresProjection,
+                    null,
+                    null,
+                    null
+                )
+
+                genreCursor?.moveToNext()
+
+                val genreName = genreCursor?.getString(0)
+                song.genre = genreName
+
+                genreCursor?.close()
+
+                return song
+            }
+        }
+
+        return null
+    }
 
     fun getAllSongs(context: Context): Array<Song> {
         var songs = ArrayList<Song>()
@@ -134,45 +273,60 @@ class PlayifyPlayer {
         return songs.toTypedArray()
     }
 
-    fun playItem(context: Context, id: String): Boolean {
-        val selection = MediaStore.Audio.Media._ID + " == " + id
+    fun setQueue(context: Context, songIDs: ArrayList<String>, startID: String?, startPlaying: Boolean?) {
+        val songs = songIDs.map<String, Song?> {
+            getSongFromMediaStore(context, it)
+        }.filterNotNull()
 
-        val projection = arrayOf(
-            MediaStore.Audio.Media.DATA,
-        )
+        songQueue = ArrayList(songs)
+        queueIndex = 0
 
-        val cursor = context?.getContentResolver()?.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            selection,
-            null,
-            null
-        )
+        player.setOnCompletionListener { onSongComplete(context) }
 
-        if (cursor != null) {
-            cursor.moveToFirst()
-            val data = cursor.getString(0)
-
-            if (data != null) {
-                player.setDataSource(data)
-                player.prepare()
-                player.start()
-                return true
+        if (startPlaying != null && startPlaying) {
+            if (startID != null) {
+                playItem(context, startID)
+            } else {
+                if (songs.first() != null) {
+                    val song = songs.first()
+                    song.songID?.let {
+                        playItem(context, it)
+                    }
+                } else {
+                    throw Exception("Could not find song!")
+                }
             }
+        }
+    }
+
+    fun playItem(context: Context, id: String, modifyQueue: Boolean = false): Boolean {
+       val song = getSongFromMediaStore(context, id)
+
+        if (song?.path != null) {
+            player.reset()
+            if (modifyQueue) {
+                songQueue = arrayListOf(song)
+                queueIndex = 0
+            }
+            player.setOnCompletionListener { onSongComplete(context) }
+            player.setDataSource(song.path)
+            player.prepare()
+            player.start()
+            return true
         }
 
         return false
     }
 
-    fun getVolume(context: Context): Int {
+    fun getVolume(context: Context): Double {
         val service = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        return service.getStreamVolume(AudioManager.STREAM_MUSIC)
+        return service.getStreamVolume(AudioManager.STREAM_MUSIC).toDouble() / volMultiplier(context)
     }
 
     fun setVolume(context: Context, volume: Double) {
         val service = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         val multiplier = volMultiplier(context)
-        val min = minVol(context)
+        val min = 0
         val myVol = (min + volume * multiplier).toInt()
         service.setStreamVolume(AudioManager.STREAM_MUSIC, myVol, AudioManager.FLAG_SHOW_UI)
     }
@@ -180,13 +334,99 @@ class PlayifyPlayer {
     private fun volMultiplier(context: Context): Int {
         val service = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         val max = service.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        val min = minVol(context)
+        val min = 0
         return max - min
     }
 
-    private fun minVol(context: Context): Int {
-        val service = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val min = service.getStreamMinVolume(AudioManager.STREAM_MUSIC)
-        return min
+    fun play() {
+        player.start()
+    }
+
+    fun pause() {
+        player.pause()
+    }
+
+    fun getPlaybackTime(): Double {
+        return player.currentPosition.toDouble()
+    }
+
+    fun setPlaybackTime(time: Int) {
+        player.seekTo(time)
+    }
+
+    fun nowPlaying(): Song? {
+        queueIndex?.let {
+            return if (it < songQueue.size) {
+                songQueue[it]
+            } else {
+                null
+            }
+        }
+        return null
+    }
+
+    fun previous(context: Context) {
+        queueIndex?.let {
+            if (it >= 0 && it < songQueue.size) {
+                songQueue[it - 1].songID?.let { id ->
+                    playItem(context, id)
+                }
+                queueIndex = queueIndex?.dec()
+            }
+        }
+    }
+
+    fun next(context: Context) {
+        queueIndex?.let {
+            if (it < songQueue.size) {
+                songQueue[it + 1].songID?.let { id ->
+                    playItem(context, id)
+                }
+                queueIndex = queueIndex?.inc()
+            }
+        }
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val channel = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        channel.lightColor = Color.BLUE
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                ""
+            }
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Playify")
+            .setContentText("Playify Music")
+            .build()
+        startForeground(1, notification)
+        return START_STICKY
+    }
+
+    private fun onSongComplete(context: Context){
+        queueIndex?.let { index ->
+            if (index + 1 < songQueue.size) {
+                songQueue[index + 1].songID?.let {
+                    playItem(context, it)
+                }
+                queueIndex = queueIndex?.inc()
+            } else {
+                queueIndex = 0
+            }
+        }
     }
 }
--- android/src/main/kotlin/com/kaya/playify/playify/PlayifyPlugin.kt
+++ android/src/main/kotlin/com/kaya/playify/playify/PlayifyPlugin.kt
@@ -1,21 +1,29 @@
 package com.kaya.playify.playify
 
 import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+
 /** PlayifyPlugin */
-class PlayifyPlugin : FlutterPlugin, MethodCallHandler {
+class PlayifyPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
     private var applicationContext: Context? = null
+
 
     private var playifyPlayer = PlayifyPlayer()
 
@@ -23,6 +31,21 @@ class PlayifyPlugin : FlutterPlugin, MethodCallHandler {
         applicationContext = flutterPluginBinding.applicationContext
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.kaya.playify/playify")
         channel.setMethodCallHandler(this)
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "com.kaya.playify/playify_status")
+        eventChannel.setStreamHandler(this)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.media.VOLUME_CHANGED_ACTION")
+        intentFilter.addAction("com.android.music.metachanged")
+        intentFilter.addAction("com.android.music.playstatechanged")
+        intentFilter.addAction("com.android.music.playbackcomplete")
+        intentFilter.addAction("com.android.music.queuechanged")
+        intentFilter.addAction("my_service")
+        //ContextWrapper(applicationContext).registerReceiver(lis, intentFilter)
+        val intent: Intent = Intent(
+            applicationContext,
+            PlayifyPlayer::class.java
+        )
+        applicationContext!!.startService(intent)
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -41,7 +64,7 @@ class PlayifyPlugin : FlutterPlugin, MethodCallHandler {
 
             id?.let { id ->
                 applicationContext?.let {
-                    val res = playifyPlayer.playItem(context = it, id = id)
+                    val res = playifyPlayer.playItem(context = it, id = id, modifyQueue = true)
                     result.success(res)
                 }
             } ?: run {
@@ -57,11 +80,53 @@ class PlayifyPlugin : FlutterPlugin, MethodCallHandler {
 
             amount?.let { volumeAmount ->
                 applicationContext?.let {
-                    val vol = playifyPlayer.setVolume(it, volumeAmount)
-                    result.success(vol)
+                    playifyPlayer.setVolume(it, volumeAmount)
+                    result.success(null)
                 }
             } ?: run {
                 result.error("ArgumentError", "Argument volume was not provided!", null)
+            }
+        } else if (call.method == "play") {
+            playifyPlayer.play()
+        } else if (call.method == "pause") {
+            playifyPlayer.pause()
+        } else if (call.method == "setQueue") {
+            val songIDs = call.argument<ArrayList<String>>("songIDs")
+            val startID = call.argument<String>("startID")
+            val startPlaying = call.argument<Boolean>("startPlaying")
+
+            songIDs?.let { ids ->
+                applicationContext?.let {
+                    playifyPlayer.setQueue(it, ids, startID, startPlaying)
+                }
+            } ?: run {
+                result.error("ArgumentError", "Argument songIDs was not provided!", null)
+            }
+        } else if (call.method == "getPlaybackTime"){
+            val duration = playifyPlayer.getPlaybackTime()
+            result.success(duration)
+        } else if (call.method == "setPlaybackTime"){
+            val time = call.argument<Double>("time")
+
+            time?.let {
+                    playifyPlayer.setPlaybackTime(it.toInt())
+            } ?: run {
+                result.error("ArgumentError", "Argument time was not provided!", null)
+            }
+        } else if (call.method == "nowPlaying") {
+            val song = playifyPlayer.nowPlaying()
+            song?.let {
+                result.success(it.toMap())
+            } ?: run {
+                result.error("PlayerError", "An error occurred when fetching song information", null)
+            }
+        } else if (call.method == "next") {
+            applicationContext?.let {
+                playifyPlayer.next(it)
+            }
+        } else if (call.method == "previous") {
+            applicationContext?.let {
+                playifyPlayer.previous(it)
             }
         } else {
             result.notImplemented()
@@ -70,5 +135,14 @@ class PlayifyPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
+    }
+
+    override fun onListen(arguments: Any?, events: EventSink?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCancel(arguments: Any?) {
+        TODO("Not yet implemented")
     }
 }
